d04054243cf8ff9c8aaf3ca7165099f95a8b5050
HomeACcessoryKid <homeaccessorykid@gmail.com>
2018-11-23 18:25:13 +0100

    udplog: switch to socket and avoid strlen-function

--- udplogger.c
+++ udplogger.c
@@ -9,30 +9,38 @@
 #include <FreeRTOS.h>
 #include <task.h>
 #include <string.h>
-#include "lwip/api.h"
+#include <lwip/sockets.h>
 
 #include <udplogger.h>
 
 char udplogstring[1450]={0}; //in the end I do not know to prevent overflow, so I use the max size of 1 UDP packet
+int  udplogstring_len=0;
 
 void udplog_send(void *pvParameters){
-    struct netconn* conn;
-    int i=0,len;
-    
+    int lSocket,i=0;
+    struct sockaddr_in sLocalAddr, sDestAddr;
+
     while (sdk_wifi_station_get_connect_status() != STATION_GOT_IP) vTaskDelay(20); //Check if we have an IP every 200ms 
-    // Create UDP connection
-    conn = netconn_new(NETCONN_UDP);
-    if (netconn_bind(   conn, IP_ADDR_ANY,       44444) != ERR_OK) netconn_delete(conn);
-    if (netconn_connect(conn, IP_ADDR_BROADCAST, 45678) != ERR_OK) netconn_delete(conn);
-    
-    while(1){
-        len=strlen(udplogstring);
-        if ((!i && len) || len>1000) {
-            struct netbuf* buf = netbuf_new();
-            void* data = netbuf_alloc(buf,len);
-            memcpy (data,udplogstring,len);
-            udplogstring[0]=0; //there is a risk of new LOG to add to string after we measured len
-            if (netconn_send(conn, buf) == ERR_OK) netbuf_delete(buf);
+
+    lSocket = lwip_socket(AF_INET, SOCK_DGRAM, 0);
+    memset((char *)&sLocalAddr, 0, sizeof(sLocalAddr));
+    memset((char *)&sDestAddr,  0, sizeof(sDestAddr));
+    /*Destination*/
+    sDestAddr.sin_family = AF_INET;
+    sDestAddr.sin_len = sizeof(sDestAddr);
+    sDestAddr.sin_addr.s_addr = htonl(INADDR_BROADCAST);
+    sDestAddr.sin_port =28338; //= 45678; //reversed bytes
+    /*Source*/
+    sLocalAddr.sin_family = AF_INET;
+    sLocalAddr.sin_len = sizeof(sLocalAddr);
+    sLocalAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    sLocalAddr.sin_port =40109; //= 44444; //reversed bytes
+    lwip_bind(lSocket, (struct sockaddr *)&sLocalAddr, sizeof(sLocalAddr));
+
+    while (1) {
+        if ((!i && udplogstring_len) || udplogstring_len>1000) {
+            lwip_sendto(lSocket, udplogstring, udplogstring_len, 0, (struct sockaddr *)&sDestAddr, sizeof(sDestAddr));
+            udplogstring_len=0;
             i=10;
         }
         if (!i) i=10; //sends output every 100ms if not more than 1000 bytes
--- udplogger.h
+++ udplogger.h
@@ -2,11 +2,12 @@
 #ifndef __UDPLOGGER_H__
 #define __UDPLOGGER_H__
 
-//use nc -kulnw0 34567 to collect this output
+//use nc -kulnw0 45678 to collect this output
 //and use     xTaskCreate(udplog_send, "logsend", 256, NULL, 4, NULL); //is prio4 a good idea??
 
-#define UDPLOG(message, ...)  sprintf(udplogstring+strlen(udplogstring),message, ##__VA_ARGS__)
+#define UDPLOG(format, ...)  udplogstring_len+=sprintf(udplogstring+udplogstring_len,format, ##__VA_ARGS__)
 void udplog_send(void *pvParameters);
 extern char udplogstring[];
+extern int udplogstring_len;
 
 #endif //__UDPLOGGER_H__
